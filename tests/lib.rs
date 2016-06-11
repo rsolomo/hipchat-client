99362478b145b43f63bcf1f377ebce5fa783f4b1
Matt Stump <mrevilgnome@gmail.com>
2016-06-11 12:50:24 -0700

    added support for users and fetching private messages (#4)

--- tests/lib.rs
+++ tests/lib.rs
@@ -11,7 +11,8 @@ use hipchat_client::Client as HipchatClient;
 struct Config {
     token: String,
     origin: String,
-    room: String
+    room: String,
+    user: u64
 }
 
 fn setup() -> (HipchatClient, Config) {
@@ -29,7 +30,7 @@ fn setup() -> (HipchatClient, Config) {
 fn integration_get_room() {
     let (client, config) = setup();
     let room = client.get_room(&config.room).unwrap();
-    println!("{:#?}", room);
+    println!("room: {:#?}", room);
     assert_eq!(room.name, config.room);
 }
 
@@ -37,6 +38,28 @@ fn integration_get_room() {
 fn integration_get_rooms() {
     let (client, config) = setup();
     let rooms = client.get_rooms(None).unwrap();
-    println!("{:#?}", rooms);
+    println!("rooms: {:#?}", rooms);
     assert!(rooms.items.iter().any(|r| r.name == config.room));
+}
+
+#[test]
+fn integration_get_user() {
+    let (client, config) = setup();
+    let user = client.get_user(&config.user.to_string()).unwrap();
+    println!("user: {:#?}", user);
+    assert_eq!(user.id, config.user);
+}
+
+#[test]
+fn integration_get_users() {
+    let (client, _) = setup();
+    let users = client.get_users(None).unwrap();
+    println!("users: {:#?}", users);
+}
+
+#[test]
+fn integration_get_private_messages() {
+    let (client, config) = setup();
+    let messages = client.get_private_messages(&config.user.to_string(), None).unwrap();
+    println!("messages: {:#?}", messages);
 }
