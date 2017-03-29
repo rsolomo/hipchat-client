540fe3faed23b6d6dbe09b8977cdc0f24e2f1462
Matthew Keibler <matt-keibler@users.noreply.github.com>
2017-03-28 20:25:31 -0400

    Upgraded hyper to 0.10 (#5)
    Upgrade involved a new tls crate, new ways to add/encode query parameters, and test coverage.

--- src/room.rs
+++ src/room.rs
@@ -1,7 +1,11 @@
 use rustc_serialize::{Decodable, Decoder};
 
-use util::{Privacy};
+use util::{Privacy, AppendToQueryParams};
 use message::{Color, MessageFormat};
+
+use url::UrlQuery;
+use url::form_urlencoded::Serializer;
+
 
 #[derive(Debug, Hash, Eq, PartialEq)]
 pub struct RoomsRequest {
@@ -9,6 +13,15 @@ pub struct RoomsRequest {
     pub max_results: Option<u64>,
     pub include_private: Option<bool>,
     pub include_archived: Option<bool>
+}
+
+impl AppendToQueryParams for RoomsRequest {
+    fn append_to(&self, query: &mut Serializer<UrlQuery>){
+        self.start_index.map(|start_index| query.append_pair("start-index", &start_index.to_string()));
+        self.max_results.map(|max_results| query.append_pair("max-results", &max_results.to_string()));
+        self.include_private.map(|include_private| query.append_pair("include-private", &include_private.to_string()));
+        self.include_archived.map(|include_archived| query.append_pair("include-archived", &include_archived.to_string()));
+    }
 }
 
 #[derive(Debug, Hash, Eq, PartialEq)]
@@ -176,6 +189,7 @@ impl Default for Notification {
 mod test {
     use super::*;
     use rustc_serialize::json;
+    use url::Url;
 
     #[test]
     fn unit_rooms_links() {
@@ -190,5 +204,33 @@ mod test {
             "next":"https://www.example.com"
         }"#).unwrap();
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn unit_default_rooms_request_should_create_empty_params(){
+        let rooms_request = RoomsRequest{ start_index: None,
+                                          max_results: None,
+                                          include_private: None,
+                                          include_archived: None };
+
+        let mut url = Url::parse("https://rsolomo.github.io/hipchat-client/hipchat_client/index.html").unwrap();
+
+        rooms_request.append_to(&mut url.query_pairs_mut());
+
+        assert_eq!(Some(""), url.query());
+    }
+
+    #[test]
+    fn unit_populated_rooms_request_should_create_encoded_params(){
+        let rooms_request = RoomsRequest{ start_index: Some(1),
+                                          max_results: Some(10),
+                                          include_private: Some(true),
+                                          include_archived: Some(true) };
+
+        let mut url = Url::parse("https://rsolomo.github.io/hipchat-client/hipchat_client/index.html").unwrap();
+
+        rooms_request.append_to(&mut url.query_pairs_mut());
+
+        assert_eq!(Some("start-index=1&max-results=10&include-private=true&include-archived=true"), url.query());
     }
 }
