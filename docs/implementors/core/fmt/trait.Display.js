6aaed048cacf74a7dcaffb9f44c21a87db6d07ca
Ray Solomon <raybsolomon@gmail.com>
2017-08-23 23:02:57 -0700

    update documentation

--- docs/implementors/core/fmt/trait.Display.js
+++ docs/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hipchat_client"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='hipchat_client/error/enum.Error.html' title='hipchat_client::error::Error'>Error</a>",];
+implementors["hipchat_client"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"hipchat_client/error/enum.Error.html\" title=\"enum hipchat_client::error::Error\">Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- docs/implementors/std/error/trait.Error.js
+++ docs/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["hipchat_client"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>StdError</a> for <a class='enum' href='hipchat_client/error/enum.Error.html' title='hipchat_client::error::Error'>Error</a>",];
+implementors["hipchat_client"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">StdError</a> for <a class=\"enum\" href=\"hipchat_client/error/enum.Error.html\" title=\"enum hipchat_client::error::Error\">Error</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
